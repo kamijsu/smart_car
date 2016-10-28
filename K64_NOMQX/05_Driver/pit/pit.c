4fc2bb5eabf0d814064b214cf180563d9b96bbe4
JunJie Zhao <790028837@qq.com>
2016-10-28 14:15:24 +0800

    2016-10-28

--- K64_NOMQX/05_Driver/pit/pit.c
+++ K64_NOMQX/05_Driver/pit/pit.c
@@ -14,7 +14,7 @@ static const IRQn_Type pit_irq[4] =
 //函数返回: 无
 //参数说明: PIT_CHx:PIT通道号，请使用宏定义
 //       ms:该通道产生中断的时间周期，单位ms，范围0-89478ms
-//功能概要: PIT模块通道初始化
+//功能概要: PIT模块通道初始化，默认关闭中断
 //==========================================================================
 void pit_init(uint8 PIT_CHx, uint32 ms) {
 	uint32 load_value;		//该通道加载值寄存器的值
@@ -24,8 +24,8 @@ void pit_init(uint8 PIT_CHx, uint32 ms) {
 	REG_SET_MASK(PIT_MCR, PIT_MCR_FRZ_MASK);		//在调试模式下定时器停止
 	load_value = PIT_WORK_FREQ * ms;	//根据产生中断的时间周期计算加载值寄存器的值
 	REG_SET_VAL(PIT_LDVAL(PIT_CHx), load_value);	//设置该通道加载值寄存器的值
-	REG_SET_MASK(PIT_TCTRL(PIT_CHx), PIT_TCTRL_TEN_MASK);	//该通道定时器时钟使能
 	REG_CLR_MASK(PIT_TCTRL(PIT_CHx), PIT_TCTRL_TIE_MASK);	//禁止该通道发送中断请求
+	REG_SET_MASK(PIT_TCTRL(PIT_CHx), PIT_TCTRL_TEN_MASK);	//该通道定时器时钟使能
 }
 
 //==========================================================================
